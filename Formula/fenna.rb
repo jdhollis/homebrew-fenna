474d1cc0f63f3e06c87e98cb08b647f28b25a018
J.D. Hollis <jd@densityofspace.com>
2022-11-20 17:47:22 -0500

    Set to v1.0.3

--- Formula/fenna.rb
+++ Formula/fenna.rb
@@ -1,9 +1,9 @@
 class Fenna < Formula
   desc "A thin Terraform wrapper for teams"
   homepage "https://github.com/jdhollis/fenna"
-  url "https://github.com/jdhollis/fenna/archive/v1.0.2.tar.gz"
-  sha256 "7a2aba7700f16a756d4441c9bd90d286860557a3db240a00840a1a4c02b8b876"
-  version "1.0.2"
+  url "https://github.com/jdhollis/fenna/archive/v1.0.3.tar.gz"
+  sha256 "bedb83a312c485d283f21da5fd9dd10ce6e0ece8d4b07a57c40cfa6fd382ed9b"
+  version "1.0.3"
 
   def install
     bin.install "fenna"
